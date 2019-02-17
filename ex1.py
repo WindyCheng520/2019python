dc1d3d02e7d8e4311854a2805a934f928e11b758
WindyCheng520 <WindyCheng@aliyun.com>
2019-02-17 22:27:06 +0800

    练习1,2

---
+++ ex1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3.7
+# -*- coding: utf-8 -*-
+print("Hello world!")
+print("Hello again")
+#print("I like typing this.")
+print("this is fun")
+print('Yay! printing')
+print("I'd much rather you 'not'. " )
+print('I "said" very good. ')
+print('你好 ')
+print("你好啊啊啊啊啊啊啊啊啊啊 ")
+
+
---
+++ ex2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3.7
+# -*- coding: utf-8 -*-
+print("I could have code like this")
+print("This will run")
+# 井号是注释
+
+
+
